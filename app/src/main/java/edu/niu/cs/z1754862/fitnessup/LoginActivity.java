af2e36ad3e60183814bb555ca571ebec501c17f2
JEngelking <jengelking13@gmail.com>
2017-04-09 23:44:51 -0500

    Data sent to server successfully, but no saving of PIN

--- app/src/main/java/edu/niu/cs/z1754862/fitnessup/LoginActivity.java
+++ app/src/main/java/edu/niu/cs/z1754862/fitnessup/LoginActivity.java
@@ -1,14 +1,161 @@
 package edu.niu.cs.z1754862.fitnessup;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
 
 public class LoginActivity extends AppCompatActivity
 {
+    TextView pinExplain;
+    EditText pinET;
+
+    public static final String PIN_MATCH = "[a-zA-Z]{3}\\d{3}";
+    public static final String APP_VERSION = "8";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar bar = getSupportActionBar();
+        bar.hide();
+
+        SharedPreferences.OnSharedPreferenceChangeListener spChanged = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Toast.makeText(getApplicationContext(), "PIN added to preferences", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(spChanged);
+
+        pinET = (EditText) findViewById(R.id.pinEditText);
+
+        pinExplain = (TextView) findViewById(R.id.pinExplainTextView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/GeosansLight.ttf");
+        pinExplain.setTypeface(typeface);
+        pinET.setTypeface(typeface);
+
+        pinET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                if (pinET.getText().toString().length() == 6)
+                {
+                    if (pinET.getText().toString().matches(PIN_MATCH))
+                    {
+                        SharedPreferences preferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("pin", pinET.getText().toString());
+                        editor.putString("ver", APP_VERSION);
+
+                        new PerformAsyncTask().execute();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    else
+                    {
+                        pinET.setText("");
+                        Toast toast = Toast.makeText(getApplicationContext(), "Please enter PIN as three letters followed by three digits", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, 0);
+                        toast.show();
+                    }
+                }
+            }
+        });
+    }
+
+    private class PerformAsyncTask extends AsyncTask<Void, Void, String>
+    {
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            String response = sendPin();
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, 0);
+            toast.show();
+        }
+    }
+
+    public String sendPin()
+    {
+        HttpURLConnection connection;
+        String response = new String();
+
+        try
+        {
+            URL url = new URL("http://students.cs.niu.edu/~exerciseapp/postcheckvalid.php");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);//set http method to post
+            connection.setRequestMethod("POST");//set to post
+            //Encode parameters to be sent
+            String outParms = "pin=" + URLEncoder.encode(pinET.getText().toString(), "UTF-8") + "&ver=" + URLEncoder.encode(APP_VERSION, "UTF-8");
+            //Set final connection attributes and create PrintWriter to get response from server
+            connection.setFixedLengthStreamingMode(outParms.getBytes().length);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            PrintWriter writer = new PrintWriter(connection.getOutputStream());
+            writer.print(outParms);
+            writer.close();
+
+            Scanner inStream = new Scanner(connection.getInputStream());
+
+            while (inStream.hasNextLine())
+            {
+                response += (inStream.nextLine());
+            }
+        }
+
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return response;
     }
 }
--- app/src/main/java/edu/niu/cs/z1754862/fitnessup/SplashScreen.java
+++ app/src/main/java/edu/niu/cs/z1754862/fitnessup/SplashScreen.java
@@ -1,21 +1,31 @@
 package edu.niu.cs.z1754862.fitnessup;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.TextView;
+import android.support.v7.app.ActionBar;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 1500;
+    TextView titleTV;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        ActionBar bar = getSupportActionBar();
+        bar.hide();
+
+        titleTV = (TextView) findViewById(R.id.titleView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/GeosansLight.ttf");
+        titleTV.setTypeface(typeface);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -24,5 +34,24 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         }, SPLASH_DISPLAY_LENGTH);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if (preferences.contains("pin"))
+        {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        else
+        {
+            Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static String getDefaultSharedPreferencesName(Context context)
+    {
+        return context.getPackageName() + "_preferences";
     }
 }
