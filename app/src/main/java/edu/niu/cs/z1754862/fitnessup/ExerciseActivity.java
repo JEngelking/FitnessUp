a221dec156598c31a6f250ca676b59aa75fa1c68
JEngelking <jengelking13@gmail.com>
2017-05-01 23:37:28 -0500

    Exercise and workout activity being sent to server.

--- app/src/main/java/edu/niu/cs/z1754862/fitnessup/ExerciseActivity.java
+++ app/src/main/java/edu/niu/cs/z1754862/fitnessup/ExerciseActivity.java
@@ -1,16 +1,32 @@
 package edu.niu.cs.z1754862.fitnessup;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class ExerciseActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
@@ -18,10 +34,13 @@ public class ExerciseActivity extends AppCompatActivity implements AdapterView.O
     Button startBtn;
     Button pauseBtn;
     Button resetBtn;
+    Button saveBtn;
     TextView timeTV;
     long startTime = 0L;
 
     private Handler handler = new Handler();
+
+    int selectedExercise;
 
     long timeInMilliseconds = 0L;
     long timeSwapBuff = 0L;
@@ -36,6 +55,7 @@ public class ExerciseActivity extends AppCompatActivity implements AdapterView.O
         startBtn = (Button) findViewById(R.id.buttonStart);
         pauseBtn = (Button) findViewById(R.id.buttonPause);
         resetBtn = (Button) findViewById(R.id.buttonReset);
+        saveBtn = (Button) findViewById(R.id.buttonSave);
 
         timeTV = (TextView) findViewById(R.id.textViewElapsedTime);
 
@@ -77,6 +97,13 @@ public class ExerciseActivity extends AppCompatActivity implements AdapterView.O
                 updatedTime = 0L;
             }
         });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new PerformAsyncTask().execute();
+            }
+        });
     }
 
     private Runnable updateTimerThread = new Runnable() {
@@ -106,7 +133,7 @@ public class ExerciseActivity extends AppCompatActivity implements AdapterView.O
 
         if (spinType == R.id.spinnerExercise)
         {
-
+            selectedExercise = exSpin.getSelectedItemPosition();
         }//end exercise spinner
     }
 
@@ -114,5 +141,76 @@ public class ExerciseActivity extends AppCompatActivity implements AdapterView.O
     public void onNothingSelected(AdapterView<?> adapterView)
     {
 
+    }
+
+    private class PerformAsyncTask extends AsyncTask<Void, Void, String>
+    {
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            String response = sendPin();
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, 0);
+            toast.show();
+
+            Intent returnToMain = new Intent(ExerciseActivity.this, MainActivity.class);
+            startActivity(returnToMain);
+        }
+    }
+
+    public String sendPin()
+    {
+        HttpURLConnection connection;
+        String response = new String();
+        String currentDT = DateFormat.getDateTimeInstance().format(new Date());
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        if (preferences.contains("pin") && preferences.contains("ver"))
+        {
+            try {
+                URL url = new URL("http://students.cs.niu.edu/~exerciseapp/postdata.php");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);//set http method to post
+                connection.setRequestMethod("POST");//set to post
+                //Encode parameters to be sent
+                String outParms = "pin=" + URLEncoder.encode(preferences.getString("pin", "def123"), "UTF-8") +
+                        "&min=" + URLEncoder.encode(Long.toString(updatedTime), "UTF-8") +
+                        "&act=" + URLEncoder.encode("1", "UTF-8") +
+                        "&name=" + URLEncoder.encode(Integer.toString(selectedExercise)) +
+                        "&dt=" + URLEncoder.encode(currentDT, "UTF-8") +
+                        "&ver=" + URLEncoder.encode(preferences.getString("ver", "UTF-8"));
+
+                //Set final connection attributes and create PrintWriter to get response from server
+                connection.setFixedLengthStreamingMode(outParms.getBytes().length);
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                PrintWriter writer = new PrintWriter(connection.getOutputStream());
+                writer.print(outParms);
+                writer.close();
+
+                Scanner inStream = new Scanner(connection.getInputStream());
+
+                while (inStream.hasNextLine())
+                {
+                    response += (inStream.nextLine());
+                }
+            }
+            catch (UnsupportedEncodingException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return response;
     }
 }
--- app/src/main/java/edu/niu/cs/z1754862/fitnessup/WorkoutActivity.java
+++ app/src/main/java/edu/niu/cs/z1754862/fitnessup/WorkoutActivity.java
@@ -1,15 +1,19 @@
 package edu.niu.cs.z1754862.fitnessup;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -23,6 +27,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class WorkoutActivity extends AppCompatActivity
 {
     private LinearLayout gallery;
@@ -30,8 +44,11 @@ public class WorkoutActivity extends AppCompatActivity
     private Button startBtn, finishBtn;
     private ProgressBar workoutPB;
 
+    int selectedWorkout;
+
     MyCountDownTimer countDownTimer;
     long timeRemaining;
+    long timeExercised;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -50,9 +67,18 @@ public class WorkoutActivity extends AppCompatActivity
 
         Intent intent = getIntent();
 
-        int selectedWorkout = intent.getIntExtra("selectedWorkout", 0);
+        selectedWorkout = intent.getIntExtra("selectedWorkout", 0);
 
         fillGallery(selectedWorkout);
+
+        finishBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new PerformAsyncTask().execute();
+            }
+        });
     }
 
     public void beginWorkoutClick(View v)
@@ -91,6 +117,8 @@ public class WorkoutActivity extends AppCompatActivity
             timeRemaining = millisUntilFinished;
 
             int progress = (int) (millisUntilFinished / 1000);
+
+            timeExercised = (1800000 - timeRemaining)/6000;
 
             workoutPB.setMax(1800000);
             workoutPB.setProgress(workoutPB.getMax() - progress);
@@ -138,5 +166,76 @@ public class WorkoutActivity extends AppCompatActivity
 
             workoutDesc.setText(WorkoutInfo.description[wew]);
         }
+    }
+
+    private class PerformAsyncTask extends AsyncTask<Void, Void, String>
+    {
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            String response = sendPin();
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, 0);
+            toast.show();
+
+            Intent returnToMain = new Intent(WorkoutActivity.this, MainActivity.class);
+            startActivity(returnToMain);
+        }
+    }
+
+    public String sendPin()
+    {
+        HttpURLConnection connection;
+        String response = new String();
+        String currentDT = DateFormat.getDateTimeInstance().format(new Date());
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        if (preferences.contains("pin") && preferences.contains("ver"))
+        {
+            try {
+                URL url = new URL("http://students.cs.niu.edu/~exerciseapp/postdata.php");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);//set http method to post
+                connection.setRequestMethod("POST");//set to post
+                //Encode parameters to be sent
+                String outParms = "pin=" + URLEncoder.encode(preferences.getString("pin", "def123"), "UTF-8") +
+                        "&min=" + URLEncoder.encode(Long.toString(timeExercised), "UTF-8") +
+                        "&act=" + URLEncoder.encode("2", "UTF-8") +
+                        "&name=" + URLEncoder.encode(Integer.toString(selectedWorkout)) +
+                        "&dt=" + URLEncoder.encode(currentDT, "UTF-8") +
+                        "&ver=" + URLEncoder.encode(preferences.getString("ver", "UTF-8"));
+
+                //Set final connection attributes and create PrintWriter to get response from server
+                connection.setFixedLengthStreamingMode(outParms.getBytes().length);
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                PrintWriter writer = new PrintWriter(connection.getOutputStream());
+                writer.print(outParms);
+                writer.close();
+
+                Scanner inStream = new Scanner(connection.getInputStream());
+
+                while (inStream.hasNextLine())
+                {
+                    response += (inStream.nextLine());
+                }
+            }
+            catch (UnsupportedEncodingException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return response;
     }
 }
